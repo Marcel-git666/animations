b2ad879488ccdc07d704e55a95a5793f7b5f5aa4
Marcel Mravec <marcel.mravec@gmail.com>
2024-05-20 15:54:39 +0200

    add assets add example1 - implicit animation

---
+++ Animations/ScaleImplicitAnimation.swift
@@ -0,0 +1,26 @@
+//
+//  ScaleImplicitAnimation.swift
+//  Animations
+//
+//  Created by Marcel Mravec on 20.05.2024.
+//
+
+import SwiftUI
+
+struct Example1: View {
+    @State private var half = false
+    @State private var dim = false
+    
+    var body: some View {
+        Image("tower")
+            .resizable()
+            .frame(width: 200, height: 400)
+            .scaleEffect(half ? 0.5 : 1.0)
+            .opacity(dim ? 0.2 : 1.0)
+            .animation(.easeInOut(duration: 1.0))
+            .onTapGesture {
+                self.dim.toggle()
+                self.half.toggle()
+            }
+    }
+}
